ca71123790974585a056b158f78057a45c8ccaa3
persianpros <persianpros@gmail.com>
2020-08-15 17:52:06 +0430

    Get rid of old webinterface support.

--- vps/src/plugin.py
+++ vps/src/plugin.py
@@ -27,26 +27,6 @@ def autostart(reason, **kwargs):
 			vps_timers.session = session
 			vps_timers.checkNextAfterEventAuto()
 			vps_timers.checkTimer()
-
-			try:
-				from Plugins.Extensions.WebInterface.WebChilds.Toplevel import addExternalChild
-				from Plugins.Extensions.WebInterface.WebChilds.Screenpage import ScreenPage
-				from twisted.web import static
-				from twisted.python import util
-				from enigma import eEnv
-			except ImportError as ie:
-				pass
-			else:
-				if hasattr(static.File, 'render_GET'):
-					class File(static.File):
-						def render_POST(self, request):
-							return self.render_GET(request)
-				else:
-					File = static.File
-
-				root = File(eEnv.resolve("${libdir}/enigma2/python/Plugins/SystemPlugins/vps/web-data"))
-				root.putChild("web", ScreenPage(session, util.sibpath(__file__, "web"), True))
-				addExternalChild(("vpsplugin", root, "VPS-Plugin", "1", False))
 		else:
 			register_vps()
 
